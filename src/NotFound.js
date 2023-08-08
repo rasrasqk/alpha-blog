24b7a5b8682d9a1dee7d8af9a3b11c643ced79c7
mohammed razi <mohammedrazir6@gmail.com>
2023-08-08 16:11:41 +0530

    first commit

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>the page is not found</p>
+        </div>
+    );
+}
+
+export default NotFound;
